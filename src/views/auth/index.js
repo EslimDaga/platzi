d073ac218395a76b5665bf47ddd38e881514d574
Eslim Daga <esliim9811@gmail.com>
2022-06-21 17:18:27 -0500

    Change login

--- src/views/auth/index.js
+++ src/views/auth/index.js
@@ -7,44 +7,44 @@ const index = () => {
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
                 <div className="rounded-t mb-0 px-6 py-6">
-                  <div className="text-center mb-3">
-                    <h6 className="text-gray-600 text-sm font-bold">
-                      Sign in with
-                    </h6>
+                  <div className="p-4">
+                    <figure>
+                      <img
+                        src="https://static.platzi.com/media/platzi-isotipo@2x.png"
+                        alt="Platzi logo"
+                        className="m-auto"
+                      />
+                    </figure>
                   </div>
-                  <hr className="mt-6 border-b-1 border-gray-400" />
                 </div>
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                  <div className="text-gray-500 text-center mb-3 font-bold">
-                    <small>Or sign in with credentials</small>
-                  </div>
                   <form>
                     <div className="relative w-full mb-3">
                       <label
-                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        className="block text-gray-700 font-urbanist text-base font-bold mb-2"
                         htmlFor="grid-password"
                       >
-                        Email
+                        Correo Electronico
                       </label>
                       <input
                         type="email"
-                        className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                        placeholder="Email"
+                        className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded font-urbanist text-base font-medium shadow focus:outline-none focus:ring w-full"
+                        placeholder="Ingrese su correo electronico"
                         style={{ transition: "all .15s ease" }}
                       />
                     </div>
 
                     <div className="relative w-full mb-3">
                       <label
-                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        className="block text-gray-700 font-urbanist text-base font-bold mb-2"
                         htmlFor="grid-password"
                       >
-                        Password
+                        Contraseña
                       </label>
                       <input
                         type="password"
-                        className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                        placeholder="Password"
+                        className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded font-urbanist text-base font-medium shadow focus:outline-none focus:ring w-full"
+                        placeholder="Ingrese su contraseña"
                         style={{ transition: "all .15s ease" }}
                       />
                     </div>
@@ -56,19 +56,19 @@ const index = () => {
                           className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
                           style={{ transition: "all .15s ease" }}
                         />
-                        <span className="ml-2 text-sm font-semibold text-gray-700">
-                          Remember me
+                        <span className="ml-2 font-urbanist text-base font-semibold text-gray-700">
+                          Recordar Sesión
                         </span>
                       </label>
                     </div>
 
                     <div className="text-center mt-6">
                       <button
-                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                        className="bg-gray-900 text-white active:bg-gray-700 text-base font-urbanist font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
                         type="button"
                         style={{ transition: "all .15s ease" }}
                       >
-                        Sign In
+                        Iniciar sesión
                       </button>
                     </div>
                   </form>
@@ -76,21 +76,13 @@ const index = () => {
               </div>
               <div className="flex flex-wrap mt-6">
                 <div className="w-1/2">
-                  <a
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                    className="text-gray-300"
-                  >
-                    <small>Forgot password?</small>
+                  <a className="text-gray-300 font-urbanist font-medium">
+                    <small>¿Olvido su contraseña?</small>
                   </a>
                 </div>
                 <div className="w-1/2 text-right">
-                  <a
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                    className="text-gray-300"
-                  >
-                    <small>Create new account</small>
+                  <a className="text-gray-300 font-urbanist font-medium">
+                    <small className="">Crea una cuenta nueva</small>
                   </a>
                 </div>
               </div>
