94692a4f8a030c8f2c09e6a82a7d5ca6087821d5
Eslim Daga <esliim9811@gmail.com>
2022-06-24 16:06:27 -0500

    Change name to the folder darkmode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeProvider } from "./contexts/darkmode/ThemeContext";
 import App from "./App";
 import "./assets/styles/index.css";
 
