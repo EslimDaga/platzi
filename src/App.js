b4cd2209e3f5eaacc0a484eaa3c36ed41ff41b5f
Eslim Daga <esliim9811@gmail.com>
2022-06-24 16:03:53 -0500

    I wrapped the app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth/context";
 import routes from "./routes/index";
 
 const App = () => {
   return (
-    <Routes>
-      {routes.map((route) => (
-        <Route
-          key={route.path}
-          path={route.path}
-          element={route.component}
-        />
-      ))}
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.component} />
+        ))}
+      </Routes>
+    </AuthProvider>
   );
 }
 
