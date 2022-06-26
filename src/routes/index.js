615eb0788edb0cfe57242ae4cea0f2141f1ab848
Eslim Daga <esliim9811@gmail.com>
2022-06-25 19:55:18 -0500

    Add route /

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
+import { Navigate } from "react-router-dom";
 import Login from "../views/auth/Login";
 import Dashboard from "../views/dashboard/index";
 
 const routes = [
+  {
+    path: "/",
+    component: <Navigate to="/login" />,
+    isPrivate: false
+  },
   {
     path: "/login",
     component: <Login />,
