7b47b03d48fd94370f5a4e2f138394a57d7c39c1
Eslim Daga <esliim9811@gmail.com>
2022-06-25 19:54:42 -0500

    Add private and public routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import PublicRoute from "./components/auth/PublicRoute";
 import { AuthProvider } from "./contexts/auth/context";
 import routes from "./routes/index";
 
@@ -6,9 +8,32 @@ const App = () => {
   return (
     <AuthProvider>
       <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.component} />
-        ))}
+        <Route element={<PrivateRoute/>}>
+          {routes.map((route) => {
+            return (
+              route.isPrivate && (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.component}
+                />
+              )
+            );
+          })}
+        </Route>
+        <Route element={<PublicRoute/>}>
+          {routes.map((route) => {
+            return (
+              !route.isPrivate && (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.component}
+                />
+              )
+            );
+          })}
+        </Route>
       </Routes>
     </AuthProvider>
   );
