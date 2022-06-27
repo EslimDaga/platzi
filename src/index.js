4db6bd25bc2993f0cdb34c9951a1d23264081d78
Eslim Daga <esliim9811@gmail.com>
2022-06-27 16:26:36 -0500

    Add layout and authProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./contexts/darkmode/ThemeContext";
 import App from "./App";
+import Layout from "./template/Layout";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth";
+import { ThemeProvider } from "./contexts/darkmode/ThemeContext";
 import "./assets/styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,7 +12,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <AuthProvider>
+          <Layout>
+            <App />
+          </Layout>
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
