efe05e3fe742c00242cdf7182e652d50dc725236
Eslim Daga <esliim9811@gmail.com>
2022-06-27 18:34:50 -0500

    Add routes the app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,47 @@
 import { Navigate } from "react-router-dom";
 import Login from "../views/auth/Login";
 import Register from "../views/auth/Register";
+import Categories from "../views/categories";
 import Dashboard from "../views/dashboard/index";
+import Products from "../views/products";
+import Users from "../views/users";
 
 const routes = [
   {
     path: "/",
     component: <Navigate to="/login" />,
-    isPrivate: false
+    isPrivate: false,
   },
   {
     path: "/login",
     component: <Login />,
-    isPrivate: false
+    isPrivate: false,
   },
   {
     path: "/register",
     component: <Register />,
-    isPrivate: false
+    isPrivate: false,
   },
   {
     path: "/dashboard",
     component: <Dashboard />,
-    isPrivate: true
-  }
+    isPrivate: true,
+  },
+  {
+    path: "/products",
+    component: <Products />,
+    isPrivate: true,
+  },
+  {
+    path: "/categories",
+    component: <Categories />,
+    isPrivate: true,
+  },
+  {
+    path: "/users",
+    component: <Users />,
+    isPrivate: true,
+  },
 ];
 
 export default routes;
