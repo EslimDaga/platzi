21976dbb3ebf1bc916935ad4864cf8deed67752e
Eslim Daga <esliim9811@gmail.com>
2022-06-27 16:19:15 -0500

    Add remove authProvider

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./components/auth/PrivateRoute";
 import PublicRoute from "./components/auth/PublicRoute";
-import { AuthProvider } from "./contexts/auth/context";
 import routes from "./routes/index";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<PrivateRoute/>}>
-          {routes.map((route) => {
-            return (
-              route.isPrivate && (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.component}
-                />
-              )
-            );
-          })}
-        </Route>
-        <Route element={<PublicRoute/>}>
-          {routes.map((route) => {
-            return (
-              !route.isPrivate && (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={route.component}
-                />
-              )
-            );
-          })}
-        </Route>
-      </Routes>
-    </AuthProvider>
+    <Routes>
+      <Route element={<PrivateRoute/>}>
+        {routes.map((route) => {
+          return (
+            route.isPrivate && (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.component}
+              />
+            )
+          );
+        })}
+      </Route>
+      <Route element={<PublicRoute/>}>
+        {routes.map((route) => {
+          return (
+            !route.isPrivate && (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.component}
+              />
+            )
+          );
+        })}
+      </Route>
+    </Routes>
   );
 }
 
