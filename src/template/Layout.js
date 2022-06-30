d533c9746093bc0523590c8d9ed5946bfcb03676
Eslim Daga <esliim9811@gmail.com>
2022-06-30 09:58:00 -0500

    Change background color in dark mode

--- src/template/Layout.js
+++ src/template/Layout.js
@@ -1,20 +1,20 @@
 import { useLocation } from "react-router-dom";
 import Header from "../components/app/Header";
 
-const Layout = (props) => {
-  let location = useLocation();
-  return (
-    <>
-      {location.pathname === "/login" || location.pathname === "/register" ? (
-        props.children
-      ) : (
-        <main className="bg-gray-100 dark:bg-gray-900 h-screen flex flex-col">
-          <Header />
-          {props.children}
-        </main>
-      )}
-    </>
-  );
-}
+const Layout = props => {
+	let location = useLocation();
+	return (
+		<>
+			{location.pathname === "/login" || location.pathname === "/register" ? (
+				props.children
+			) : (
+				<main className="bg-gray-100 dark:bg-[#282b42] h-screen flex flex-col">
+					<Header />
+					{props.children}
+				</main>
+			)}
+		</>
+	);
+};
 
 export default Layout;
