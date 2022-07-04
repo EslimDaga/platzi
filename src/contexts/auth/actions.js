2124339f4d15944a4af597598fb8dd0d44a24136
Eslim Daga <esliim9811@gmail.com>
2022-07-04 12:26:10 -0500

    Refactor action

--- src/contexts/auth/actions.js
+++ src/contexts/auth/actions.js
@@ -1,29 +1,24 @@
-import { api } from "../../constants/global";
+import { Login } from "../../services/auth";
 
 export async function loginUser(dispatch, loginPayload) {
-	const requestOptions = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(loginPayload),
-	};
-
 	try {
 		dispatch({ type: "REQUEST_LOGIN" });
-		let response = await fetch(`${api}/auth/login`, requestOptions);
-		let data = await response.json();
+		const { email, password } = loginPayload;
+		let response = await Login(email, password).then(function (response) {
+			const { data } = response;
+			if (data.access_token) {
+				dispatch({ type: "LOGIN_SUCCESS", payload: data.access_token });
+				const { access_token } = data;
+				localStorage.setItem("currentToken", JSON.stringify(access_token));
+				return data;
+			}
 
-		if (data.access_token) {
-			dispatch({ type: "LOGIN_SUCCESS", payload: data.access_token });
-			const { access_token } = data;
-			localStorage.setItem("currentToken", JSON.stringify(access_token));
-			return data;
-		}
-
-		dispatch({
-			type: "LOGIN_ERROR",
-			error: "Usuario y/o contraseña incorrectos",
+			dispatch({
+				type: "LOGIN_ERROR",
+				error: "Usuario y/o contraseña incorrectos",
+			});
 		});
-		return;
+		return response;
 	} catch (error) {
 		dispatch({ type: "LOGIN_ERROR", error: "Error en la aplicación" });
 	}
