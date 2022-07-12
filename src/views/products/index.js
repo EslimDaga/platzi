28d4e21c1bb6a6b44fc28cf6db94416898a36d5a
Eslim Daga <esliim9811@gmail.com>
2022-07-12 11:02:55 -0500

    Add breadcumb component

--- src/views/products/index.js
+++ src/views/products/index.js
@@ -1,7 +1,14 @@
+import Breadcumb from "../../components/app/Breadcumb";
+
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+	return (
+		<div className="flex-1 overflow-y-auto w-full">
+			<Breadcumb link="products" title="Productos" />
+			<div className="relative bg-lightBlue-600 pt-8">
+				<div className="w-full px-4 rounded-xl">Jeje</div>
+			</div>
+		</div>
+	);
+};
 
 export default Products;
