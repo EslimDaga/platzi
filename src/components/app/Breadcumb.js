c2da0779053ffc0b64fa0a8a326dc1dc869532b7
Eslim Daga <esliim9811@gmail.com>
2022-07-12 11:02:35 -0500

    Add breadcumb component file

---
+++ src/components/app/Breadcumb.js
@@ -0,0 +1,34 @@
+import { FaAngleRight, FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Breadcumb = ({ link, title }) => {
+	return (
+		<div className="relative pt-8">
+			<nav className="flex px-4" aria-label="Breadcrumb">
+				<ol className="inline-flex items-center space-x-1 md:space-x-3">
+					<li className="inline-flex items-center">
+						<Link
+							to="/dashboard"
+							className="text-gray-700 dark:text-white text-base font-urbanist font-semibold hover:text-gray-900 inline-flex items-center"
+						>
+							<FaHome />
+						</Link>
+					</li>
+					<li className="ml-2">
+						<div className="flex items-center">
+							<FaAngleRight className="text-gray-700 dark:text-white" />
+							<Link
+								to={`/` + link}
+								className="text-gray-700 dark:text-white text-base font-urbanist font-semibold hover:text-gray-900 ml-1 md:ml-2"
+							>
+								{title}
+							</Link>
+						</div>
+					</li>
+				</ol>
+			</nav>
+		</div>
+	);
+};
+
+export default Breadcumb;
