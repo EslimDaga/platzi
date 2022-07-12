3005396a5502d930c9714a1afbc553616d4e66f7
Eslim Daga <esliim9811@gmail.com>
2022-07-12 11:02:12 -0500

    Refactor breadcumd

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import Breadcumb from "../../components/app/Breadcumb";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Pagination } from "swiper";
 import {
-	FaAngleRight,
 	FaBoxes,
 	FaBoxOpen,
-	FaHome,
 	FaMoneyBillAlt,
 	FaMoneyCheckAlt,
 	FaUsers,
@@ -74,31 +72,7 @@ const Dashboard = () => {
 	};
 	return (
 		<div className="flex-1 overflow-y-auto w-full">
-			<div className="relative pt-8">
-				<nav className="flex px-4" aria-label="Breadcrumb">
-					<ol className="inline-flex items-center space-x-1 md:space-x-3">
-						<li className="inline-flex items-center">
-							<Link
-								to="/dashboard"
-								className="text-gray-700 dark:text-white text-base font-urbanist font-semibold hover:text-gray-900 inline-flex items-center"
-							>
-								<FaHome />
-							</Link>
-						</li>
-						<li className="ml-2">
-							<div className="flex items-center">
-								<FaAngleRight className="text-gray-700 dark:text-white" />
-								<Link
-									to="/dashboard"
-									className="text-gray-700 dark:text-white text-base font-urbanist font-semibold hover:text-gray-900 ml-1 md:ml-2"
-								>
-									Dashboard
-								</Link>
-							</div>
-						</li>
-					</ol>
-				</nav>
-			</div>
+			<Breadcumb link="dashboard" title="Dashboard" />
 			<div className="relative bg-lightBlue-600 pt-8">
 				<div className="mx-auto w-full">
 					<div>
