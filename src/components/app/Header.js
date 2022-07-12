0f5673a199a9278a7658452f1eb20a160ef48a01
Eslim Daga <esliim9811@gmail.com>
2022-07-12 16:22:50 -0500

    Change padding

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
 	};
 	return (
 		<Popover className="relative bg-white dark:bg-[#181B32] shadow-xl">
-			<div className="w-full mx-auto px-6 sm:px-6">
+			<div className="w-full mx-auto px-4 sm:px-4">
 				<div className="flex justify-between items-center py-4 md:space-x-10">
 					<div className="flex justify-start">
 						<a href="/">
