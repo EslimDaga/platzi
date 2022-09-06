6457e0694967015e6a863f016fb4a70162202c50
Eslim Daga <esliim9811@gmail.com>
2022-09-05 20:24:22 -0500

    The form add product is okay, and form edit is building

--- src/views/products/index.js
+++ src/views/products/index.js
@@ -3,7 +3,7 @@ import { Formik } from "formik";
 import { AgGridReact } from "ag-grid-react";
 import { ThemeContext } from "../../contexts/darkmode/ThemeContext";
 import { FaPen, FaPlusCircle, FaTimes, FaTrash } from "react-icons/fa";
-import { getProducts } from "../../services/products";
+import { getProduct, getProducts } from "../../services/products";
 import { AG_GRID_LOCALE_ES } from "../../i18n/locale.es";
 import { useDropzone } from "react-dropzone";
 import Breadcumb from "../../components/app/Breadcumb";
@@ -12,14 +12,17 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import "animate.css";
 import { getCategoriesForSelect } from "../../services/categories";
+import axios from "axios";
 
 const Products = () => {
 	const { theme } = useContext(ThemeContext);
 
 	const [showModalCreateProduct, setShowModalCreateProduct] = useState(false);
+	const [showModalEditProduct, setShowModalEditProduct] = useState(false);
 	const [fileName, setFileName] = useState([]);
-	const [categories, setCategories] = useState([]);
 	const [rowData, setRowData] = useState();
+	const [categories, setCategories] = useState([]);
+	const [product, setProduct] = useState({});
 
 	const { acceptedFiles, getRootProps, getInputProps, fileRejections } =
 		useDropzone({
@@ -75,11 +78,21 @@ const Products = () => {
 			cellRenderer: params => {
 				return (
 					<div className="flex justify-center">
-						<button className="flex items-center text-white font-bold bg-blue-900 rounded-md px-3 mr-2">
+						<button
+							onClick={() => {
+								openModalEditProduct(params.value);
+							}}
+							className="flex items-center text-white font-bold bg-blue-900 rounded-md px-3 mr-2"
+						>
 							<FaPen className="mr-2" />
 							Editar
 						</button>
-						<button className="flex items-center text-white font-bold bg-red-500 rounded-md px-3">
+						<button
+							onClick={() => {
+								handleDelete(params.value);
+							}}
+							className="flex items-center text-white font-bold bg-red-500 rounded-md px-3"
+						>
 							<FaTrash className="mr-2" />
 							Eliminar
 						</button>
@@ -114,6 +127,15 @@ const Products = () => {
 		setShowModalCreateProduct(true);
 	};
 
+	const openModalEditProduct = id => {
+		console.log(id);
+		getProduct(id).then(data => {
+			console.log(data);
+			setProduct(data);
+		});
+		setShowModalEditProduct(true);
+	};
+
 	const closeModalCreateProduct = () => {
 		document
 			.getElementById("modal-create-product")
@@ -124,7 +146,20 @@ const Products = () => {
 
 		setTimeout(() => {
 			setShowModalCreateProduct(false);
-		}, 250);
+		}, 150);
+	};
+
+	const closeModalEditProduct = () => {
+		document
+			.getElementById("modal-edit-product")
+			.classList.remove("animate__fadeInDown");
+		document
+			.getElementById("modal-edit-product")
+			.classList.add("animate__fadeOutUp");
+
+		setTimeout(() => {
+			setShowModalEditProduct(false);
+		}, 150);
 	};
 
 	const validateLogin = values => {
@@ -160,7 +195,29 @@ const Products = () => {
 
 	const handleSubmit = values => {
 		values["image"] = fileName;
-		console.log(values);
+		const { name, description, price, category, image } = values;
+		axios
+			.post("https://api.escuelajs.co/api/v1/products/", {
+				title: name,
+				price,
+				description,
+				categoryId: category,
+				images: image,
+			})
+			.then(res => {
+				if (res.status === 201) {
+					setShowModalCreateProduct(false);
+					setRowData([...rowData, res.data]);
+				}
+			});
+	};
+
+	const handleDelete = id => {
+		axios.delete(`https://api.escuelajs.co/api/v1/products/${id}`).then(res => {
+			if (res.status === 200) {
+				setRowData(rowData.filter(item => item.id !== id));
+			}
+		});
 	};
 
 	const customStylesWhiteMode = {
@@ -235,6 +292,293 @@ const Products = () => {
 									<button
 										className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
 										onClick={closeModalCreateProduct}
+									>
+										<span className="bg-transparent text-gray-500 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100 h-6 w-6 text-xl block outline-none focus:outline-none">
+											<FaTimes className="mt-1" />
+										</span>
+									</button>
+								</div>
+								<div className="relative">
+									<div className="w-full">
+										<div className="bg-white dark:bg-[#282b42] rounded-lg shadow-sm">
+											<Formik
+												initialValues={{
+													name: "",
+													description: "",
+													category: "",
+													price: "",
+													image: "",
+												}}
+												validate={validateLogin}
+												onSubmit={handleSubmit}
+											>
+												{({
+													values,
+													errors,
+													handleChange,
+													setFieldValue,
+													handleSubmit,
+												}) => (
+													<form onSubmit={handleSubmit}>
+														<div className="gap-6 px-6 pb-6">
+															<div className="justify-center w-full">
+																<div className="w-full">
+																	<div className="space-y-6">
+																		<div>
+																			<label
+																				htmlFor="name"
+																				className="block text-gray-700 dark:text-white font-urbanist text-base font-bold mb-2"
+																			>
+																				Nombre
+																			</label>
+																			<input
+																				type="text"
+																				name="name"
+																				id="name"
+																				onChange={handleChange}
+																				value={values.name}
+																				className={
+																					"border-0 px-3 py-3 placeholder-gray-400 text-gray-700 dark:text-gray-100 bg-[#EEEEEE] dark:bg-[#3F425E] rounded-md font-urbanist text-base font-medium shadow focus:outline-gray-700 w-full" +
+																					(errors.name
+																						? " border-red-500 border-[1px] focus:outline-red-500"
+																						: "")
+																				}
+																			/>
+																			{errors.name && (
+																				<label
+																					className="text-[#e6215d] text-sm font-urbanist font-semibold"
+																					htmlFor="error-alert-for-name"
+																				>
+																					{errors.name}
+																				</label>
+																			)}
+																		</div>
+																		<div>
+																			<label
+																				htmlFor="description"
+																				className="block text-gray-700 dark:text-white font-urbanist text-base font-bold mb-2"
+																			>
+																				Descripción
+																			</label>
+																			<textarea
+																				name="description"
+																				id="description"
+																				onChange={handleChange}
+																				value={values.description}
+																				cols="30"
+																				rows="3"
+																				className={
+																					"border-0 px-3 py-3 placeholder-gray-400 text-gray-700 dark:text-gray-100 bg-[#EEEEEE] dark:bg-[#3F425E] rounded-md font-urbanist text-base font-medium shadow focus:outline-gray-700 w-full" +
+																					(errors.description
+																						? " border-red-500 border-[1px] focus:outline-red-500"
+																						: "")
+																				}
+																			></textarea>
+																			{errors.description && (
+																				<label
+																					className="text-[#e6215d] text-sm font-urbanist font-semibold"
+																					htmlFor="error-alert-for-description"
+																				>
+																					{errors.description}
+																				</label>
+																			)}
+																		</div>
+																		<div>
+																			<label
+																				htmlFor="description"
+																				className="block text-gray-700 dark:text-white font-urbanist text-base font-bold mb-2"
+																			>
+																				Categoría
+																			</label>
+																			<Select
+																				options={categories}
+																				name="category"
+																				value={categories.find(
+																					option =>
+																						option.value === values.category
+																				)}
+																				onChange={option => {
+																					setFieldValue(
+																						"category",
+																						option.value
+																					);
+																				}}
+																				placeholder="Selecciona una categoría"
+																				className={
+																					"w-full rounded-lg z-1 focus:shadow font-urbanist font-medium text-base focus:outline-gray-700" +
+																					(errors.category
+																						? " border-red-500 border-[1px] focus:outline-red-500"
+																						: "")
+																				}
+																				styles={
+																					theme === "light"
+																						? customStylesWhiteMode
+																						: customStylesDarkMode
+																				}
+																			/>
+																			{errors.category && (
+																				<label
+																					className="text-[#e6215d] text-sm font-urbanist font-semibold"
+																					htmlFor="error-alert-for-category"
+																				>
+																					{errors.category}
+																				</label>
+																			)}
+																		</div>
+																		<div>
+																			<label
+																				htmlFor="price"
+																				className="block text-gray-700 dark:text-white font-urbanist text-base font-bold mb-2"
+																			>
+																				Precio
+																			</label>
+																			<input
+																				type="number"
+																				name="price"
+																				id="price"
+																				onChange={handleChange}
+																				value={values.price}
+																				className={
+																					"border-0 px-3 py-3 placeholder-gray-400 text-gray-700 dark:text-gray-100 bg-[#EEEEEE] dark:bg-[#3F425E] rounded-md font-urbanist text-base font-medium shadow focus:outline-gray-700 w-full" +
+																					(errors.price
+																						? " border-red-500 border-[1px] focus:outline-red-500"
+																						: "")
+																				}
+																			/>
+																			{errors.price && (
+																				<label className="text-[#e6215d] text-sm font-urbanist font-semibold">
+																					{errors.price}
+																				</label>
+																			)}
+																		</div>
+																		<div
+																			{...getRootProps()}
+																			className={
+																				"bg-[#EEEEEE] dark:bg-[#3F425E] flex justify-center items-center rounded-md border-2 border-dashed px-6 pt-5 pb-6 cursor-pointer" +
+																				(acceptedFiles.length > 0
+																					? " border-green-500"
+																					: "") +
+																				(errors.image ? " border-red-500" : "")
+																			}
+																		>
+																			<div className="space-y-1 text-center font-urbanist">
+																				<svg
+																					className="mx-auto h-12 w-12 text-gray-400"
+																					stroke="currentColor"
+																					fill="none"
+																					viewBox="0 0 48 48"
+																					aria-hidden="true"
+																				>
+																					<path
+																						d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+																						strokeWidth={2}
+																						strokeLinecap="round"
+																						strokeLinejoin="round"
+																					/>
+																				</svg>
+																				<div className="flex text-gray-600">
+																					<label
+																						htmlFor="image"
+																						className="relative cursor-pointer rounded-md font-bold text-blue-600 dark:text-[#98ca3f] focus-within:outline-none focus-within:ring-2 focus-within:ring-transparent hover:text-blue-500"
+																					>
+																						<span>Carga un archivo</span>
+																						<input
+																							{...getInputProps()}
+																							onChange={handleChange}
+																							id="image"
+																							name="image"
+																							type="file"
+																							value={values.image}
+																						/>
+																					</label>
+																					<p className="pl-1 text-gray-500 dark:text-gray-100">
+																						o arrastralo y sueltalo
+																					</p>
+																				</div>
+																				<p className="text-sm text-gray-500 dark:text-gray-300">
+																					PNG, JPG tamaño maximo 10MB
+																				</p>
+																				<p className="text-sm text-gray-500 dark:text-gray-300">
+																					(Máximo 3 archivos)
+																				</p>
+																			</div>
+																		</div>
+																		{acceptedFiles.length < 1 && (
+																			<div className="text-[#e6215d] text-sm font-urbanist font-semibold">
+																				{errors.image}
+																			</div>
+																		)}
+																		{acceptedFiles.length > 0 &&
+																			acceptedFiles.map(file => (
+																				<li
+																					key={file.path}
+																					className="font-urbanist font-bold text-base text-gray-700 dark:text-gray-100"
+																				>
+																					{file.path} - {file.size} bytes
+																				</li>
+																			))}
+																		{fileRejections.length > 0 &&
+																			fileRejections.map(({ file, errors }) => (
+																				<li
+																					key={file.path}
+																					className="font-urbanist font-bold text-base text-gray-700 dark:text-gray-100"
+																				>
+																					{file.path} - {file.size} bytes
+																					<ul>
+																						{errors.map(e => (
+																							<li
+																								key={e.code}
+																								className="font-urbanist font-bold text-base text-red-500"
+																							>
+																								El tipo de archivo debe ser
+																								imagen/jpeg, imagen/png.
+																							</li>
+																						))}
+																					</ul>
+																				</li>
+																			))}
+																		<div>
+																			<button
+																				className={`bg-[#98ca3f] text-gray-900 active:bg-gray-700 text-base font-urbanist font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 w-full`}
+																				type="submit"
+																				style={{ transition: "all .15s ease" }}
+																			>
+																				<div className="inline-flex items-center">
+																					Guardar
+																				</div>
+																			</button>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</form>
+												)}
+											</Formik>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+				</>
+			)}
+			{showModalEditProduct && (
+				<>
+					<div
+						id="modal-edit-product"
+						className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none animate__animated animate__fadeInDown"
+					>
+						<div className="relative w-full h-full py-6 px-4 max-w-3xl">
+							<div className="my-auto border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-[#282b42] outline-none focus:outline-none">
+								<div className="flex items-start justify-between px-6 py-5 border-blueGray-200 rounded-t">
+									<h3 className="dark:text-gray-100 text-base font-urbanist font-bold self-center">
+										Editar Producto
+									</h3>
+									<button
+										className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+										onClick={closeModalEditProduct}
 									>
 										<span className="bg-transparent text-gray-500 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100 h-6 w-6 text-xl block outline-none focus:outline-none">
 											<FaTimes className="mt-1" />
