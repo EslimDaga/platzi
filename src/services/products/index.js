5f3bec9009a4c2ff06f1904c19946cd53d13d2bf
Eslim Daga <esliim9811@gmail.com>
2022-10-03 20:30:36 -0500

    Add update product service

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -30,3 +30,23 @@ export const createProduct = async (
 	const data = response;
 	return data;
 };
+
+export const updateProduct = async (
+	id,
+	name,
+	description,
+	category,
+	price,
+	image
+) => {
+	const response = await axios.put(`${api}/products/${id}`, {
+		id: id,
+		title: name,
+		price,
+		description,
+		category: category.id,
+		images: image,
+	});
+	const data = response;
+	return data;
+};
